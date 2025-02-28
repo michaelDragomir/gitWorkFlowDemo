414f05e7876769cef32a71d44d13f46a8ab8210d
Michael Dragomir <michaeldragomir@gmail.com>
2025-02-28 11:40:26 -0800

    updated state to 10

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = useState(10);
 
 	const countHandler = () => {
 		setCount((count) => count + 5);
