b18337bb09b4aa4f99955c95f6ec5807673ee7c9
Michael Dragomir <michaeldragomir@gmail.com>
2025-04-09 15:33:09 -0700

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
 	const [count, setCount] = useState(10);
 
-	const countHandler = () => {
-		setCount((count) => count + 5);
-	};
+	// const countHandler = () => {
+	// 	setCount((count) => count + 5);
+	// };
 
 	return (
 		<>
-			<h1>My cool counter update</h1>
+			<h1>hello tech 37....</h1>
 			<div className='card'>
 				<button onClick={countHandler}>count IS NOW! {count}</button>
 			</div>
